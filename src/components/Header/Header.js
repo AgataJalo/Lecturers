13c0acbe7afc7faacb645632fc62715677b1ef7e
Agata <agatajalowska@gmail.com>
2019-09-17 00:09:57 +0200

    buttons hover

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,20 +4,20 @@ import './Header.scss'
  class Header extends Component {
     render() {
         return (
-            <h2>
-            <div className="letter">W</div>
-            <div className="letter">y</div>
-            <div className="letter">k</div>
-            <div className="letter">ł</div>
-            <div className="letter">a</div>
-            <div className="letter">d</div>
-            <div className="letter">o</div>
-            <div className="letter">w</div>
-            <div className="letter">c</div>
-            <div className="letter">y</div>
-            <div className="letterIT">I</div>
-            <div className="letterIT">T</div>
-        </h2>
+            <div className ="header">
+                    <div className="letter">W</div>
+                    <div className="letter">y</div>
+                    <div className="letter">k</div>
+                    <div className="letter">ł</div>
+                    <div className="letter">a</div>
+                    <div className="letter">d</div>
+                    <div className="letter">o</div>
+                    <div className="letter">w</div>
+                    <div className="letter">c</div>
+                    <div className="letter">y</div>
+                    <div className="letterIT">I</div>
+                    <div className="letterIT">T</div>
+        </div>
         )
     }
 }
